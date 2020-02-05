4b30a00220c188a6284d3f4fcd0ad4bd234cd5e5
Josh <140453a@acadiau.ca>
2020-02-05 11:45:40 -0400

    Added photos model for the flickr data.

---
+++ backend/models/Photos.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const PhotoSchema = mongoose.Schema({
+  photoid: {
+    type: Integer,
+    required: true
+  },
+  owner: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+module.exports = mongoose.model('Photos', PhotoSchema);
