90d8f2a89ad1180db308ac64f30fe6a594bc1b9f
Josh <140453a@acadiau.ca>
2020-02-07 21:59:10 -0400

    Finally got exif data in json form. Now just have to add it to DB.

--- backend/api_helper.js
+++ backend/api_helper.js
@@ -1,4 +1,46 @@
-// Function to iteratate through json from /getPopular and extract exif data.
-// module.export{
-// function(json)
-//}
+const express = require('express');
+const fetch = require('node-fetch');
+const async = require('async');
+const KEY = process.env.FLICKR_KEY;
+
+async function savePopular(req, res){
+  try{
+    let test = await fetch(`https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&nojsoncallback=1&per_page=3&api_key=${KEY}`,
+    {method: "GET",
+    headers: {}});
+    const data = await test.json();
+    exif_data = await getExif(data);
+    //console.log(exif_data)
+    return await exif_data
+  }catch(err) {console.log(err)}
+}
+
+
+async function getExif(data){
+  try{
+    var id_arr = [];
+     Object.keys(data.photos.photo).forEach((item, i) => {
+      let photoid = data.photos.photo[i].id;
+      id_arr[i] = `https://www.flickr.com/services/rest/?method=flickr.photos.getExif&photo_id=${photoid}&format=json&nojsoncallback=1&api_key=${KEY}`
+      });
+
+
+      var promises = id_arr.map(url => fetch(url).then(y => y.text()));
+      return Promise.all(promises)
+  .catch(error => console.log(`Error in promises ${error}`))
+
+
+  }catch(err) {console.log(err)}
+
+}
+
+
+
+async function savePhoto(photo) {
+  let savedPhoto = await photo.save();
+  res.json(savedPhoto);
+}
+
+
+
+module.exports  = {savePhoto, savePopular};
--- backend/routes/resp.js
+++ backend/routes/resp.js
@@ -1,39 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
-const KEY = process.env.FLICKR_KEY;
+const helper = require('../api_helper.js')
+
 
 router.get('/getPopular', async (req, res) => {
-  try{
-    let test = await fetch(`https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&nojsoncallback=1&api_key=${KEY}`,
-    {method: "GET",
-    headers: {}});
-    const data = await test.json();
-    Object.keys(data.photos.photo).forEach((item, i) => {
-      console.log(data.photos.photo[i]); // each individual post.
-    });
+  let exif = await helper.savePopular();
+  let exif_j = []
+  for (var i = 0; i < exif.length; i++) {
+    exif_j[i] = JSON.parse(exif[i])
+}
 
-    res.send("200");
-  }catch(err) {res.json({ message: err})}
-  })
+  res.json(exif_j);
 
-// submits photos
-router.post('/', async (req, res) => {
-    const photo = new Photo({
-      photoid: req.body.photoid,
-      owner: req.body.owner,
-      title: req.body.title
-    });
+});
 
-    try{
-    const savedPhoto = await photo.save();
-    res.json(savedPhoto);
-  }catch(err) {
-    res.json({ message: err});
-  }
-  });
 
 
 module.exports = router;
 
 //TODO: Store every individual post above into the mongo DB.
+
+
+// const express = require('express');
+// const router = express.Router();
+// const fetch = require('node-fetch');
+// const helper = require('../api_helper.js')
+// const KEY = process.env.FLICKR_KEY;
+//
+// router.get('/getPopular', async (req, res) => {
+//   try{
+//     let test = await fetch(`https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&format=json&nojsoncallback=1&api_key=${KEY}`,
+//     {method: "GET",
+//     headers: {}});
+//     const data = await test.json();
+//     Object.keys(data.photos.photo).forEach((item, i) => {
+//       var photo = new Photo({
+//         photoid: data.photos.photo[i].id,
+//         owner: data.photos.photo[i].owner,
+//         title: data.photos.photo[i].title
+//       })
+//
+//         try{
+//           savePhoto();
+//         }catch(err) {res.json({ message: err})};
+//       //console.log(data.photos.photo[i]); // each individual post.
+//     });
+//     res.send("200");
+//
+//   }catch(err) {res.json({ message: err})}
+//   })
+//
+//
+//
+// module.exports = router;
+//
+// //TODO: Store every individual post above into the mongo DB.
